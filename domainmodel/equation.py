e550e51ed5f63136ac8ea0813a6555f9b8ae19f0
vyacheslav_slava <devnull@localhost>
2015-06-23 12:45:12 +0300

    changed equation.py

--- domainmodel/equation.py
+++ domainmodel/equation.py
@@ -17,8 +17,14 @@ class Equation(object):
         self.name = edict["Name"]
         self.vars = edict["Vars"]
         self.system = edict["System"]
-        self.params = edict["Params"]
+		if len(edict["Params"]) != 0 and len(edict["ParamValues"]) == 0:
+		    raise AttributeError("The system has some parameters, but their values was not specified!")
+		if len(edict["Params"]) == 0 and len(edict["ParamValues"]) != 0:
+		    raise AttributeError("The system hasn't any parameters, but the field ParamValues is not empty!")
+		self.params = edict["Params"]
         self.paramValues = edict["ParamValues"]
+		if len(self.paramValues) > 1:
+			self.defaultParamsIndex = edict["DefaulParamsIndex"]
    
     def getPropertiesDict(self):          
         propDict = OrderedDict([            
