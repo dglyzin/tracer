dda18008ca8b4282cc81c8654a72b9c046f9041d
vyacheslav_slava <devnull@localhost>
2015-08-10 14:53:53 +0300

    Еще попытка исправления

--- domainmodel/newFuncGenerator.py
+++ domainmodel/newFuncGenerator.py
@@ -737,7 +737,7 @@ class generator2D(abstractGenerator):
             someLen = yfrom
             lenOfConnection = yto - yfrom
             secondIndex = 'idxY'
-            stepAlongSide = self.gridStep[1]
+            stepAlongSide = self.gridStep[0]
         elif mainBlockSide == 1:
             xfrom = mainBlock.sizeX + mainBlock.offsetX
             xto = mainBlock.sizeX + mainBlock.offsetX
@@ -746,7 +746,7 @@ class generator2D(abstractGenerator):
             someLen = yfrom
             lenOfConnection = yto - yfrom
             secondIndex = 'idxY'
-            stepAlongSide = self.gridStep[1]
+            stepAlongSide = self.gridStep[0]
         elif mainBlockSide == 2:
             xfrom = max([mainBlock.offsetX, secBlock.offsetX]) - mainBlock.offsetX
             xto = min([mainBlock.offsetX + mainBlock.sizeX, secBlock.offsetX + secBlock.sizeX]) - mainBlock.offsetX
@@ -755,7 +755,7 @@ class generator2D(abstractGenerator):
             someLen = xfrom
             lenOfConnection = xto - xfrom
             secondIndex = 'idxX'
-            stepAlongSide = self.gridStep[0]
+            stepAlongSide = self.gridStep[1]
         else:
             xfrom = max([mainBlock.offsetX, secBlock.offsetX]) - mainBlock.offsetX
             xto = min([mainBlock.offsetX + mainBlock.sizeX, secBlock.offsetX + secBlock.sizeX]) - mainBlock.offsetX
@@ -764,7 +764,7 @@ class generator2D(abstractGenerator):
             someLen = xfrom
             lenOfConnection = xto - xfrom
             secondIndex = 'idxX'
-            stepAlongSide = self.gridStep[0]
+            stepAlongSide = self.gridStep[1]
         return InterconnectRegion(firstIndex, secondIndex, mainBlockSide, stepAlongSide, someLen, lenOfConnection, xfrom, xto, yfrom, yto, self.blocks.index(secBlock))
    
     def generateFillInitValFuncsForAllBlocks(self, listWithInitialFunctionNames, listWithDirichletFunctionNames):
@@ -813,7 +813,7 @@ class generator2D(abstractGenerator):
     
     def createListOfBCondsForSide(self, block, blockNumber, side):
         if side == 2:
-            cellLen = self.gridStep[0]
+            cellLen = self.gridStep[1]
             sideMaxRange = block.sizeX
             sideMinRange = 0.0
             currentSideValue = 0.0
@@ -821,7 +821,7 @@ class generator2D(abstractGenerator):
             stepFrom = lambda eqRegion: eqRegion.xfrom
             stepTo = lambda eqRegion: eqRegion.xto
         elif side == 3:
-            cellLen = self.gridStep[0]
+            cellLen = self.gridStep[1]
             sideMaxRange = block.sizeX
             sideMinRange = 0.0
             currentSideValue = block.sizeY
@@ -829,7 +829,7 @@ class generator2D(abstractGenerator):
             stepFrom = lambda eqRegion: eqRegion.xfrom
             stepTo = lambda eqRegion: eqRegion.xto
         elif side == 0:
-            cellLen = self.gridStep[1]
+            cellLen = self.gridStep[0]
             sideMaxRange = block.sizeY
             sideMinRange = 0.0
             currentSideValue = 0.0
@@ -837,7 +837,7 @@ class generator2D(abstractGenerator):
             stepFrom = lambda eqRegion: eqRegion.yfrom
             stepTo = lambda eqRegion: eqRegion.yto
         elif side == 1:
-            cellLen = self.gridStep[1]
+            cellLen = self.gridStep[0]
             sideMaxRange = block.sizeY
             sideMinRange = 0.0
             currentSideValue = block.sizeX
