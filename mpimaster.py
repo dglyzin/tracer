6b62c61fdaac5d3d5bb1533220992de96874bd8e
dglyzin <devnull@localhost>
2015-10-19 20:23:49 +0300

    mpi connection with core

--- mpimaster.py
+++ mpimaster.py
@@ -42,6 +42,11 @@ def start_serving(args, geometry, dimension):
     size = world.Get_size()
 
     user_status = np.zeros(1, dtype="int32")
+    comp_status = np.zeros(1, dtype="int32")
+    lastStepAccepted = np.zeros(1, dtype="int32")
+    timeStep = np.zeros(1, dtype="float64")
+    problemTime = np.zeros(1, dtype="float64")
+    readyToSave = np.zeros(1, dtype="int32")
     
     #user_status[0] = USER_STATUS_START
     user_status[0] = USER_STATUS_STOP
@@ -59,7 +64,40 @@ def start_serving(args, geometry, dimension):
 
     #1.
     world.Bcast([user_status, MPI.INT], root=0)
-    #world.Recv([data, MPI.DOUBLE], source=idx, tag = 0)
+    world.Recv([comp_status, MPI.INT], source=1, tag = 0)
+    
+    #main computing cycle
+    while (user_status[0] != USER_STATUS_STOP) and (comp_status[0] != JS_RUNNING):
+        world.Recv([lastStepAccepted, MPI.INT], source=1, tag = 0)
+        world.Recv([timeStep, MPI.DOUBLE], source=1, tag = 0)
+        world.Recv([problemTime, MPI.DOUBLE], source=1, tag = 0)
+
+
+        world.Recv([readyToSave, MPI.INT], source=1, tag = 0)
+        if (readyToSave[0] == 1):
+            print "PM: time to save but nothing I can do so far"
+            #core decided it's saving time
+            #we should receive all the data and save it
+            #also save pictures and filename to database
+            #world.Recv([data, MPI.DOUBLE], source=idx, tag = 0)    
+            pass
+
+        
+        #todo check status in the db
+        world.Bcast([user_status, MPI.INT], root=0)
+        world.Recv([comp_status, MPI.INT], source=1, tag = 0)
+        #todo store state to db
+        
+        
+    #end of computing cycle    
+    if (user_status[0] != USER_STATUS_STOP):
+        print "Leaving main cycle with user status ", user_status[0]
+    if (comp_status[0] != JS_RUNNING):
+        print "Leaving main cycle with job state ", comp_status[0]
+
+
+
+
 
 
 if __name__ == '__main__':
