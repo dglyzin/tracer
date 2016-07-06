1dd979fd4e8723e6f136704040ab4ba4e54fea2f
Daniil Frolov <FrolovD94@gmail.com>
2016-07-06 15:49:21 +0300

    apeedTest.sh added

---
+++ speedTest.sh
@@ -0,0 +1,121 @@
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_100.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_100.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_100.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_100.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_200.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_400.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_500.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1200.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1600.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1600.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1600.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1600.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1800.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1800.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1800.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_1800.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_2000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_2000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_2000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_2000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_4000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_4000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_4000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_4000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_5000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_5000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_5000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_5000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_10000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_10000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_10000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_1_10000.json -debug
+
+
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_100.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_100.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_100.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_100.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_200.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_400.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_500.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1500.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_1500.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2200.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2200.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2400.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_2400.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_4000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_4000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_4000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_4000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_5000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_5000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_5000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_5000.json -debug
+
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_10000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_10000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_10000.json -debug
+python remoterun.py fr_work_connection.json tests/speedTest/heat_2_10000.json -debug
