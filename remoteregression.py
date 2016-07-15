d596a6fedb427df8ec9d2d3e55cea069de1d047f
Dmitry Glyzin <glyzin@gmail.com>
2016-07-15 13:50:12 +0300

    remoteregression done

--- remoteregression.py
+++ remoteregression.py
@@ -3,6 +3,9 @@
 запуск тестов регрессии на удаленном кластере
 
 Аргументы:
+connection
+  json-файл с параметрами подключения
+
 параметры:
 -folder 
 запуск тестов из подпапки, по умолчанию выполняются все тесты из regression/
@@ -10,12 +13,12 @@
 -nodecount=4
 будут запущены только те тесты, для которых требуется меньше или равно nodecount узлов
 
+-norun 
+запуск только сравнений, на случай если все уже посчитано
+
 подключаемся, запускаем regression.py с заданными параметрами
 копируем назад лог тестирования
 '''
-remoteRunScriptName='project.sh'
-remoteProjectFileName='project.json'
-remoteMp4Name = 'project.mp4'
 
 import json
 import os
@@ -24,126 +27,49 @@ import getpass
 import paramiko
 import argparse
 from collections import OrderedDict
-
-
-
-class Connection(object):
-    def __init__(self):
-        self.host = "corp7.uniyar.ac.ru"
-        self.port = 2222
-        self.username = "tester"
-        self.password = ""
-        self.workspace = "/home/tester/Tracer"
-        self.tracerFolder = "/home/dglyzin/Tracer"        
-
-    def toDict(self):
-        connDict = OrderedDict([
-        ("Host", self.host),
-        ("Port", self.port),
-        ("Username", self.username),
-        ("Password", self.password),
-        ("Workspace", self.workspace),
-        ("TracerFolder", self.tracerFolder)
-        ])
-        return connDict
-
-    def fromDict(self, connDict):
-        self.host = connDict["Host"]
-        self.port = connDict["Port"]
-        self.username = connDict["Username"]
-        self.password = connDict["Password"]
-        self.workspace = connDict["Workspace"]
-        self.tracerFolder = connDict["TracerFolder"]
+from remoterun import Connection
         
-
-        
-def remoteProjectRun(connection, inputFile, continueEnabled, continueFnameProvided, continueFileName, jobId, finishTimeProvided, finishTime, debug, projectFolder):
+def remoteRegressionRun(connection, args):
     '''
-      connection: file with connection settings
-      inputFile:  project file
-      continueEnabled: true if user wants to continue from computed file 
-      continueFnameProvided: true if user wants to continue from specific file, false if the last computed file to be used
-      continueFileName:
-      jobId: id of the task in the db
-      finishTimeProvided: true if user wants to override json value for finish time
-      finishTime:
-      debug: true if user wants to run small problem (10 min. max)
-      projectFolder: defaults to json file name unless specified
+      connection: dictionary with connection settings
+      args.nodecount:  skip tests that require more nodes
+      args.folder : relative to connection.tracerfolder/hybriddomain/
+      args.norun: do not run computations
+      args.debug: use debug partition (does not work now as everything goes to debug)
     '''
     
     #prepare command line argumetnts for preprocessor
-    optionalArgs=''    
-    if not (jobId is None):
-        optionalArgs+=" -jobId "+str(jobId)
-    if finishTimeProvided:
-        optionalArgs+=" -finish "+str(finishTime)
-    if continueEnabled:
-        optionalArgs+=" -cont"
-        if continueFnameProvided:
-            optionalArgs+=" "+continueFileName
-    if debug:
-        optionalArgs+=" -debug"
+    optionalArgs=" -nodecount " + str(args.nodecount) + " -folder " + args.folder    
     
+    if args.norun:
+        optionalArgs+=" -norun"
+    if args.debug:
+        optionalArgs+=" -debug"    
     
     #get project file name without extension
-    print inputFile
-    if projectFolder is None:
-        projectPathName, _ = os.path.splitext(inputFile)   
-        projectFolder = os.path.basename(projectPathName)
+    domainFolder = connection.tracerFolder+"/hybriddomain"
+    regscript = connection.tracerFolder+"/hybriddomain/regression.py"
+    regfolder = connection.tracerFolder+"/hybriddomain/"+args.folder
+    remoteRegLog = regfolder+"/regression.log" 
     
-
+    print "Running regression script {} on the remote machine with parameters {}".format(regscript, optionalArgs)
+    
+    localRegLog = "remoteregression.log"
+    
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     #print conn.host, conn.username, passwd, conn.port
     try:
         client.connect(hostname=connection.host, username=connection.username, password=connection.password, port=connection.port )
 
-        print "Checking if folder "+connection.workspace+" exists..."
-        stdin, stdout, stderr = client.exec_command('test -d '+connection.workspace)
+        print "Checking if folder "+regfolder+" exists..."
+        stdin, stdout, stderr = client.exec_command('test -d '+regfolder)
         if stdout.channel.recv_exit_status():
-            print "Please create workspace folder and put hybriddomain preprocessor into it"
+            print "Please specify existing folder with tests"
             return
         else:
-            print "Workspace OK."
+            print "Regfolder OK."
 
-        projFolder = connection.workspace+"/"+projectFolder
-        print "Creating/cleaning project folder: "
-        stdin, stdout, stderr = client.exec_command('test -d  '+projFolder)
-        if stdout.channel.recv_exit_status():
-            stdin, stdout, stderr = client.exec_command('mkdir  '+projFolder)
-            print "Folder created."
-        else:
-            if not continueEnabled:  
-                stdin, stdout, stderr = client.exec_command('rm -rf '+projFolder+'/*')
-                stdout.read()
-                print "Folder cleaned."                
-            else:
-                print "Folder exists, no cleaning needed."
-                #now check if file to continue from exists
-                if continueFnameProvided:
-                    print "Checking if file to continue from  ("+continueFileName+") exists..."
-                    stdin, stdout, stderr = client.exec_command('test -f '+continueFileName)
-                    if stdout.channel.recv_exit_status():
-                        print "File not found, please specify existing file to continue"
-                        return
-                    else:
-                        print "File OK."
-                                      
-        cftp=client.open_sftp()
-        cftp.put(inputFile, projFolder+"/"+remoteProjectFileName)
-        cftp.close()
-        
-        #3 Run jsontobin on json
-        print '\nRunning preprocessor:'
-        command = 'python '+connection.tracerFolder+'/hybriddomain/jsontobin.py '+ projFolder+'/'+remoteProjectFileName + " " + connection.tracerFolder
-        
-        print command, optionalArgs
-        stdin, stdout, stderr = client.exec_command(command+optionalArgs)
-        print stdout.read()
-        print "jsontobin stderr:"
-        print stderr.read()
-        print "stderr END"
-        #4 Run Solver binary on created files
         print "Checking if solver executable at "+connection.tracerFolder+"/hybridsolver/bin/HS exists..."
         stdin, stdout, stderr = client.exec_command('test -f '+connection.tracerFolder + "/hybridsolver/bin/HS")
         if stdout.channel.recv_exit_status():
@@ -151,14 +77,21 @@ def remoteProjectRun(connection, inputFile, continueEnabled, continueFnameProvid
             return
         else:
             print "Solver executable found."
-
-        stdin, stdout, stderr = client.exec_command('sh '+projFolder+'/'+remoteRunScriptName)
+        
+        #3 Run jsontobin on json
+        print '\nRunning regression script:'
+        command = "cd "+domainFolder+'; python ' + regscript 
+        
+        print "executing ", command, optionalArgs
+        stdin, stdout, stderr = client.exec_command(command+optionalArgs)
         print stdout.read()
+        print "regtest stderr:"
         print stderr.read()
+        print "stderr END"
         
         #get resulting files
-        cftp=client.open_sftp()
-        cftp.get(projFolder+"/"+remoteMp4Name, projectPathName+".mp4")
+        cftp=client.open_sftp()        
+        cftp.get(remoteRegLog, localRegLog)
         cftp.close()
         
         client.close()
@@ -188,36 +121,18 @@ def getGonnection(connFileName):
     return connection
 
 
-def finalParseAndRun(connection, inputFileName, args, projectFolder=None):
-    finishTimeProvided = not (args.finish is None)
-    continueFileName = args.cont  
-    continueEnabled = not (continueFileName is None)
-    continueFnameProvided =  not (continueFileName == "/") if continueEnabled else False
        
-    remoteProjectRun(connection, inputFileName, continueEnabled, continueFnameProvided, continueFileName, args.jobId, finishTimeProvided, args.finish, args.debug, projectFolder)
-
-
 if __name__=='__main__':    
     parser = argparse.ArgumentParser(description='Processing json file on a remote cluster.', epilog = "Have fun!")
     parser.add_argument('connFileName', type = str, help = "local json file with connection info")    
-    #mandatory argument, json filename
-    parser.add_argument('projectFileName', type = str, help = "local json file to process")
-    #optional argument, unique job Id for identification in database
-    parser.add_argument('-jobId', type = int, help = "unique job ID") 
-    #optional argument, exactly one float to override json finish time
-    parser.add_argument('-finish', type=float, help = "new finish time to override json value")
-    #optional argument with one or no argument, filename to continue computations from
-    #if no filename is provided with this option, the last state is taken
-    parser.add_argument('-cont', nargs='?', const="/", type=str, help = "add this flag if you want to continue existing solution.\n Provide specific remote filename or the last one will be used. ")
-    parser.add_argument('-debug', help="add this flag to run program in debug partition", action="store_true")
-    
+    parser.add_argument('-nodecount', type = int, default=4, help = "Number of available nodes to run tests on.")    
+    parser.add_argument('-folder', type = str, default="regression", help = "Folder to search tests in (including subfolders).")    
+    parser.add_argument('-norun', help="Do not make runs, only compare existing data.", action="store_true")
+    parser.add_argument('-debug', help="add this flag to run program in debug partition", action="store_true")    
     args = parser.parse_args()
     
     connFileName = args.connFileName    
     connection = getGonnection(connFileName)
-
-    inputFileName = args.projectFileName
-   
-    finalParseAndRun(connection, inputFileName, args)
+    remoteRegressionRun(connection, args)
 
 
