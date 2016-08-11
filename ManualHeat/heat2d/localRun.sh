15cbda12b9aa9e6298a24e9eb3a05c1210ae2b38
Chester94 <devnull@localhost>
2016-08-11 11:13:50 +0300

    some echo in localRun

--- ManualHeat/heat2d/localRun.sh
+++ ManualHeat/heat2d/localRun.sh
@@ -1,7 +1,14 @@
 path=`pwd`
 export LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH
 
+echo "\nSimple"
 ./heat2dSimple 0 1 0.001 0.001 0.0000001
+
+echo "\nWith function in this file"
 ./heat2dWithFunc 0 1 0.001 0.001 0.0000001
+
+echo "\nWith function in another file"
 ./heat2dDepend 0 1 0.001 0.001 0.0000001
+
+echo "\nWith lib.so"
 ./heat2dLib 0 1 0.001 0.001 0.0000001
