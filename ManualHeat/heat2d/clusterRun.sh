9fa290fc1041910b854fe80fbea2d51090554a57
Chester94 <devnull@localhost>
2016-08-11 11:39:59 +0300

    some fix in clusterRun

--- ManualHeat/heat2d/clusterRun.sh
+++ ManualHeat/heat2d/clusterRun.sh
@@ -1,7 +1,14 @@
 path=`pwd`
 export LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH
 
-srun -N1 -p debug heat2dSimple 0 1 1.0 0.001 0.0000001
-srun -N1 -p debug heat2dWithFunc 0 1 1.0 0.001 0.0000001
-srun -N1 -p debug heat2dDepend 0 1 0.1.0 0.001 0.0000001
-srun -N1 -p debug heat2dLib 0 1 1.0 0.001 0.0000001
+echo "\nSimple"
+srun -N1 -p debug heat2dSimple 0 1 0.01 0.001 0.0000001
+
+echo "\nWith function in this file"
+srun -N1 -p debug heat2dWithFunc 0 1 0.01 0.001 0.0000001
+
+echo "\nWith function in another file"
+srun -N1 -p debug heat2dDepend 0 1 0.01 0.001 0.0000001
+
+echo "\nWith lib.so"
+srun -N1 -p debug heat2dLib 0 1 0.01 0.001 0.0000001
