b4c40d803411001c727f9b1f029ab4d3dc312b8c
Dmitry Glyzin <glyzin@gmail.com>
2017-03-02 13:10:25 +0300

    cleanup

--- domainmodel/tester1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#from funcGenerator import FunctionCodeGenerator
-from model import Model
-
-mod = Model()
-mod.loadFromFile("../tests/test3_heat_wbounds0.json")
-mod.createCPP("FunctionsNew.cpp")
-print "Done! See FunctionsNew.cpp"
