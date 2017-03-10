b11db0ca1fe9b2c0eb922510de8a3729288266db
Frolov Daniil <devnull@localhost>
2017-03-10 12:10:50 +0300

    сравнение результатов вычислений

--- fileDiff.py
+++ fileDiff.py
@@ -3,6 +3,8 @@ import filecmp
 import numpy as np
 import logging
 
+from domainmodel.binaryFileReader import getBinaryData
+
 compareFailed = False
 compareOk = True
 
@@ -48,10 +50,20 @@ if __name__ == "__main__":
        simple bytewise comparison
     '''
     argc = len(sys.argv)
+    if ((argc < 7) or (argc % 2 != 1)):
+        print "Arguments error.\nave tolerance, max tolerance, dom, bin, dom, bin [, dom, bin]"
+        exit(0)
+    
+    dataList = []
+    
+    for i in range(3, argc - 1, 2):
+        data = getBinaryData(sys.argv[i], sys.argv[i+1]);
+        dataList.append(data);
 
-    for i in range(1, argc - 1):
-        for j in range(i + 1, argc):
-            if filecmp.cmp(sys.argv[i], sys.argv[j], shallow=False):
-                print "Test OK! " + sys.argv[i] + " " + sys.argv[j]
+    for i in range(0, len(dataList)):
+        for j in range(i + 1, len(dataList)):
+            #if filecmp.cmp(sys.argv[i], sys.argv[j], shallow=False):
+            if compareData(dataList[i], dataList[j], {"ave" : sys.argv[1], "max" : sys.argv[2]}):
+                print "Test OK! " + sys.argv[2*i + 4] + " " + sys.argv[2*j + 4]
             else:
-                print "TEST FAILED! " + sys.argv[i] + " " + sys.argv[j]
+                print "TEST FAILED! " + sys.argv[2*i + 4] + " " + sys.argv[2*j + 4]
