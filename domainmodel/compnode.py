d1b5a738e5eac7f210b8510d08308061c89002e7
Dmitry Glyzin <glyzin@gmail.com>
2017-06-19 18:38:10 +0300

    changing hardware format

--- domainmodel/compnode.py
+++ domainmodel/compnode.py
@@ -9,21 +9,34 @@ from collections import OrderedDict
 
 '''
 Description of hardware resources available for core launch.
+Memory is in Gigabytes
 '''
 class Compnode(object):
     def __init__(self):
         self.name = "cnode1"
         self.cpuCount = 1
+        self.cpuMemory = [1]
         self.gpuCount = 0
+        self.cpuMemory = []
         
     def fillProperties(self,ndict):
         self.name = ndict["Name"]
         self.cpuCount = ndict["CpuCount"]
+        try:
+            self.cpuMemory = ndict["CpuMemory"]
+        except:
+            pass
         self.gpuCount = ndict["GpuCount"]
+        try:
+            self.gpuMemory = ndict["GpuMemory"]
+        except:
+            pass
         
     def getPropertiesDict(self):          
         return OrderedDict([
                             ("Name", self.name),
                             ("CpuCount", self.cpuCount),
-                            ("GpuCount", self.gpuCount)                   
+                            ("CpuMemory", self.cpuMemory),
+                            ("GpuCount", self.gpuCount) 
+                            ("GpuMemory", self.gpuMemory),                 
                             ])
