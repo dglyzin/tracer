cef48fa79033b65d27d94482b7c732a34693160e
valdecar <valdecar@ya.ru>
2017-07-28 11:36:12 +0300

    add singleModel for run_to_file/_html add parser tests: run_tests_for_parser

--- tests/tester.py
+++ tests/tester.py
@@ -5,14 +5,26 @@ https://stackoverflow.com/a/22517624
 '''
 
 import unittest
+
+# for parser
+from domainmodel.criminal.tests import ParserException
 from domainmodel.criminal.tests import test_powers
-from domainmodel.criminal.tests_gen import test_templates_1d
-from introduction.part_2_generators import test_model_create_cpp
+from domainmodel.criminal.tests import test_diff_outs
+from domainmodel.criminal.tests import test_diff_parser
+from domainmodel.criminal.tests import test_bounds
+
+# for cpp generation
 from introduction.part_2_generators import test_cpp, GccException
+# old
+from introduction.part_2_generators import test_model_create_cpp
+# new
+from domainmodel.criminal.tests_gen import test_templates_1d
+
+# for logger
 from introduction.part_2_generators import test_logger as test_logger_from_submodule
 
-import domainmodel.criminal.tests as ts
 import os
+import sys
 import logger_std
 
 from TextTestRunner import SimpleTextRunner
@@ -22,28 +34,9 @@ logFileName = 'tests/tester.log'
 loggerName = __name__
 
 logger = logger_std.create_logger(loggerName, logFileName, 'CRITICAL')
+    
 
-
-def test_logger():
-    '''
-    DESCRIPTION:
-    Write log into console and
-    file "tests/tester.log"
-    It also cath loger from submodule
-    and write it data if needed.
-    See:
-    https://docs.python.org/2.7/howto/logging-cookbook.html#using-logging-in-multiple-modules
-    '''
-
-    logger.info("logger info")
-    logger.debug("logger debug")
-    logger.error("logger error")
-
-    # add log from submodule
-    test_logger_from_submodule()
-
-
-def set_tests_for_class(_class, dim, genType='old'):
+def set_tests_for_class(_class, dim, genType='old', singleModel=None):
     '''
     DESCRIPTION:
     Fill _class by tests from folder tests/{dim}dTests
@@ -56,6 +49,8 @@ def set_tests_for_class(_class, dim, genType='old'):
 
     INPUT:
     genType - 'old' or 'new' generator will be used.
+    singeModel - name of particlular model in "dim + 'dTests'"
+                 directory.
     '''
     logger.debug("FROM set_tests_for_class")
 
@@ -93,10 +88,18 @@ def set_tests_for_class(_class, dim, genType='old'):
     if len(ds) == 0:
         raise BaseException("test folder for dimension %d is empty" % dim)
 
-    # remove all except .json
-    # and add full path
-    modelsP = dict([(d.split('.')[0], os.path.join(curDir, testFolder, d))
-                    for d in ds if d.split('.')[-1] == 'json'])
+    if singleModel is not None:
+        # use single test
+        if len(singleModel.split('.')) == 1:
+            singleModel = singleModel + '.json'
+        modelsP = dict([(singleModel.split('.')[0],
+                         os.path.join(curDir, testFolder, singleModel))])
+    else:
+        # remove all except .json
+        # and add full path
+        modelsP = dict([(d.split('.')[0], os.path.join(curDir, testFolder, d))
+                        for d in ds if d.split('.')[-1] == 'json'])
+
     logger.debug("modlels paths = %s" % str(modelsP))
     # END FOR
 
@@ -148,6 +151,14 @@ class CppGenTestCases(unittest.TestCase):
         This function generate json to cpp and
         run gcc at it.
 
+        success:
+        if cpp_generator or test_cpp generate no error
+        fail:
+        if cpp_generator or test_cpp generate some error
+        error:
+        if some error in parser arrive.
+
+
         It is beter to writing exception for each
         test that needed and cath them here.
         If No exception arrive then test passed
@@ -166,7 +177,6 @@ class CppGenTestCases(unittest.TestCase):
         '''
         try:
             # generate cpp
-            #test_model_create_cpp(modelFileName)
             self.cpp_generator(modelFileName)
             modelName = os.path.basename(modelFileName).split('.')[0]+'.cpp'
 
@@ -205,9 +215,89 @@ class CppGenTestCases(unittest.TestCase):
             self.assertTrue(False)  # msg
 
 
-def run_to_file(dim=1, genType='old'):
+class ParserTestCases(unittest.TestCase):
+    '''
+    DESCRIPTION:
+    Test cases for new parser.
+    '''
+    
+    def setUp(self):
+        '''
+        DESCRIPTION:
+        Called automatically before all tests.
+        '''
+        pass
 
-    set_tests_for_class(CppGenTestCases, dim, genType)
+    def tearDown(self):
+        '''
+        DESCRIPTION:
+        Called automatically after all tests.
+        '''
+        pass
+
+    def test_powers(self):
+        self.core(test_powers)
+        
+    def test_diff_outs(self):
+        self.core(test_diff_outs)
+
+    def test_diff_parser(self):
+        self.core(test_diff_parser)
+
+    def test_bounds(self):
+        self.core(test_bounds)
+    
+    def core(self, test_func):
+        '''
+        DESCRIPTION:
+        Core for all tests of this class.
+
+        This function launch test test_func.
+
+        success:
+        if test_func generate no error
+        fail:
+        if test_func generate some error
+        error:
+        if some error in parser arrive.
+
+        It is beter to writing exception for each
+        test that needed and cath them here.
+        If No exception arrive then test passed
+        else then logging them and call assertTrue(False)
+        for saving it with test name in result.
+        
+        Also it is better to add DESCRIPTION tag for
+        each except block.
+
+        INPUT:
+        test_func - test to launch.
+        '''
+        try:
+            test_func()
+                        
+            # tell that test successful
+            self.assertTrue(True)
+
+        except ParserException as e:
+            '''
+            DESCRIPTION:
+            Exception in parser.
+            '''
+            # tell that test failure
+            self.assertTrue(False)  # msg
+            
+        except BaseException as be:
+            '''
+            DESCRIPTION:
+            All other exceptions
+            '''
+            logger.error("unknown error in some of parser tests")
+
+
+def run_to_file(dim=1, genType='old', singleModel=None):
+
+    set_tests_for_class(CppGenTestCases, dim, genType, singleModel)
 
     _suite = unittest.TestLoader().loadTestsFromTestCase(CppGenTestCases)
 
@@ -216,8 +306,8 @@ def run_to_file(dim=1, genType='old'):
     return(result)
 
 
-def run_to_html(dim=1, genType='old'):
-    set_tests_for_class(CppGenTestCases, dim, genType)
+def run_to_html(dim=1, genType='old', singleModel=None):
+    set_tests_for_class(CppGenTestCases, dim, genType, singleModel)
     _suite = unittest.TestLoader().loadTestsFromTestCase(CppGenTestCases)
     
     buf = open('tests/tester_raport.html', 'w')
@@ -228,7 +318,61 @@ def run_to_html(dim=1, genType='old'):
     )
     runner.run(_suite)
     buf.close()
+
     
+def run_tests_for_parser():
+
+    _suite = unittest.TestLoader().loadTestsFromTestCase(ParserTestCases)
+
+    result = SimpleTextRunner(verbosity=3, buffer=True).run(_suite)
+
+    return(result)
+
+
+def test_exception_inner():
+    try:
+        fifo(x)
+    except:
+        raise(TestException(name='test'))
+
+
+def test_exception():
+    try:
+        test_exception_inner()
+    except TestException as e:
+        print(e.name)
+        print(e.data)
+
+
+class TestException(Exception):
+    '''
+    DESCRIPTION:
+    For cathing error of gcc.
+    For tests cases in tester.py.
+    '''
+    def __init__(self, name):
+        self.name = name
+        self.data = sys.exc_info()
+
+
+def test_logger():
+    '''
+    DESCRIPTION:
+    Write log into console and
+    file "tests/tester.log"
+    It also cath loger from submodule
+    and write it data if needed.
+    See:
+    https://docs.python.org/2.7/howto/logging-cookbook.html#using-logging-in-multiple-modules
+    '''
+
+    logger.info("logger info")
+    logger.debug("logger debug")
+    logger.error("logger error")
+
+    # add log from submodule
+    test_logger_from_submodule()
+
 
 if __name__ == '__main__':
     suite().run(unittest.defaultTestResult)
